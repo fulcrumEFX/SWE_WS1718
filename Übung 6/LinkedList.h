70bf3b1ee030078904a856282e5ba15a84bf45c0
fulcrumEFX <tobiaspfaff1@gmail.com>
2017-11-15 08:58:31 +0100

    update

--- Übung 6/LinkedList.h
+++ Übung 6/LinkedList.h
@@ -1,5 +1,4 @@
 ﻿#pragma once
-#include "Component.h"
 
 template <typename T>
 class CLinkedList
@@ -10,33 +9,39 @@ private:
 		T data;
 	};
 	node *head;
+	
 public:
 	CLinkedList();
 	~CLinkedList();
 	void add(T _t);
 	void print(void);
 	T * search(T _t);
+	/*void del(T _t);*/
+
 };
 
 template <typename T>
-CLinkedList<T>::CLinkedList()
+inline CLinkedList<T>::CLinkedList()
 {
 	head = NULL;
 }
+
 template <typename T>
-CLinkedList<T>::~CLinkedList()
+inline CLinkedList<T>::~CLinkedList()
 {
 }
+
 template <typename T>
-void CLinkedList<T>::add(T _t)
+inline void CLinkedList<T>::add(T _t)
 {
 	node *n = new node;				//neuen Node erstellen
 	n->data = _t;					//data von neuem Node mit �bergebenen Daten f�llen
 	n->next = head;					//next pointer auf n�chstes Element setzen
 	head = n;						//head auf neuen Start der Liste setzen
 }
+
 template <typename T>
-void CLinkedList<T>::print(void)
+inline void CLinkedList<T>::print(void)
 {
 	node *temp = new node;
 	temp = head;
@@ -46,13 +51,31 @@ void CLinkedList<T>::print(void)
 		temp = temp->next;
 	}
 }
+
+
 template <typename T>
-T * CLinkedList<T>::search(T _t)
+inline T * CLinkedList<T>::search(T _t)
 {
-	CComponent *ptr;
+	T *ptr;
 	for (ptr = head; ptr != NULL; ptr = ptr->next)
 	{
-
+		if (ptr->data = _t)
+			return ptr;
 	}
+	return NULL;
 }
+//
+//template<typename T>
+//inline void CLinkedList<T>::del(T _t)
+//{
+//	CLinkedList<T>.search(_t);
+//	node **ptr = &head;			//Pointer to a pointer
+//	while (ptr != _t) {
+//		if (entry->data == _t)
+//			ptr = &(*ptr)->next;
+//		}
+//	*ptr = _t->next;
+//	
+//}
+//
 
