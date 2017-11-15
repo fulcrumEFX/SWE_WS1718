3fa5abb95da03e4531b946ff53cb3b812a9c8444
fulcrumEFX <tobiaspfaff1@gmail.com>
2017-11-15 15:12:24 +0100

    template update

--- Übung 6/LinkedList.h
+++ Übung 6/LinkedList.h
@@ -1,23 +1,24 @@
 ﻿#pragma once
 
 template <typename T>
+class node {
+public:
+	node *next;
+	T data;
+};
+
+template <typename T>
 class CLinkedList
 {
 private:
-	struct node {
-		node *next;
-		T data;
-	};
-	node *head;
-	
+	node<T> *head;
 public:
 	CLinkedList();
 	~CLinkedList();
 	void add(T _t);
 	void print(void);
-	T * search(T _t);
-	/*void del(T _t);*/
-
+	node<T>* CLinkedList<T>::find(T _t);
+	void CLinkedList<T>::del(T _t);
 };
 
 template <typename T>
@@ -29,21 +30,28 @@ inline CLinkedList<T>::CLinkedList()
 template <typename T>
 inline CLinkedList<T>::~CLinkedList()
 {
+	node<T> * current = head;
+	while (current != NULL) {
+		node<T> * next = current->next;
+		delete current;
+		current = next;
+	}
+	head = 0;
 }
 
 template <typename T>
 inline void CLinkedList<T>::add(T _t)
 {
-	node *n = new node;				//neuen Node erstellen
-	n->data = _t;					//data von neuem Node mit �bergebenen Daten f�llen
-	n->next = head;					//next pointer auf n�chstes Element setzen
-	head = n;						//head auf neuen Start der Liste setzen
+	node<T> *n = new node<T>;
+	n->data = _t;			
+	n->next = head;	
+	head = n;						
 }
 
 template <typename T>
 inline void CLinkedList<T>::print(void)
 {
-	node *temp = new node;
+	node<T> *temp = new node<T>;
 	temp = head;
 	while (temp != NULL)
 	{
@@ -52,30 +60,50 @@ inline void CLinkedList<T>::print(void)
 	}
 }
 
-
 template <typename T>
-inline T * CLinkedList<T>::search(T _t)
+inline node<T>* CLinkedList<T>::find(T _t)
 {
-	T *ptr;
+	node<T> *ptr = NULL;
 	for (ptr = head; ptr != NULL; ptr = ptr->next)
 	{
-		if (ptr->data = _t)
+		if (ptr->data.getName() == _t.getName()){
 			return ptr;
+		}
 	}
 	return NULL;
 }
-//
-//template<typename T>
-//inline void CLinkedList<T>::del(T _t)
+template<typename T>
+inline void CLinkedList<T>::del(T _t)
+{
+	node<T> *delPtr = find(_t);
+	node<T> **ptr = &head;
+	while ((*ptr)->next != delPtr) {
+		ptr = &(*ptr)->next;
+	}
+	(*ptr)->next = delPtr->next;
+
+}
+
+
+
+
+//template <typename T>
+//inline bool CLinkedList<T>::operator==(const CLinkedList<T> &rhs) const
 //{
-//	CLinkedList<T>.search(_t);
-//	node **ptr = &head;			//Pointer to a pointer
-//	while (ptr != _t) {
-//		if (entry->data == _t)
-//			ptr = &(*ptr)->next;
-//		}
-//	*ptr = _t->next;
-//	
+//	node<T> *lhsTemp = head;
+//	node<T> *rhsTemp = rhs.head;
+//	while (lhsTemp != NULL || rhsTemp != NULL)
+//	{
+//		if (lhsTemp != NULL && rhsTemp == NULL || lhsTemp == NULL && rhsTemp != NULL)
+//			return false;
+//		else if (lhsTemp->data != rhsTemp->data)
+//			return false;
+//		lhsTemp = lhsTemp->next;
+//		rhsTemp = rhsTemp->next;
+//	}
+//	return true;
 //}
-//
+
+
+
 
