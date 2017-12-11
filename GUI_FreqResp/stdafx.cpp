6c686a67c1bd8a5b7a812eca11369ab93958ba07
fulcrumEFX <tobiaspfaff1@gmail.com>
2017-12-11 14:51:00 +0100

    removed some files

--- GUI_FreqResp/stdafx.cpp
+++
@@ -1,8 +0,0 @@
-
-// stdafx.cpp : Quelldatei, die nur die Standard-Includes einbindet.
-// GUI_FreqResp.pch ist der vorkompilierte Header.
-// stdafx.obj enthält die vorkompilierten Typinformationen.
-
-#include "stdafx.h"
-
-
